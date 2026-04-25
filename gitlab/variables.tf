fd28152ad1512a76f93969993faf35ab4a45f3cb
dimar <dimaremenec@gmail.com>
2026-04-25 13:06:54 +0300

    added variables

---
+++ gitlab/variables.tf
@@ -0,0 +1,20 @@
+variable "cloud_id" {
+  type        = string
+  description = "ID облака"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "ID директории в облаке"
+}
+
+variable "zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Зона доступности"
+
+  validation {
+    condition     = contains(["ru-central1-a", "ru-central1-b", "ru-central1-d"], var.zone)
+    error_message = "Недопустимое значение зоны"
+  }
+}
